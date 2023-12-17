429aaa566cdf0cb5e355a070c94c51949f986fe9
Dmytro Pogrebniak <dima.squash123@gmail.com>
2023-12-17 13:46:00 +0800

    Initial Commit

---
+++ TherapyApp/TherapyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TherapyAppApp.swift
+//  TherapyApp
+//
+//  Created by Dmytro Pogrebniak on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TherapyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
